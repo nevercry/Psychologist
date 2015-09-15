f6ff861ef0628ff90ebc35d4ebb31ea276f5846a
nevercry <ysfnevercry@gmail.com>
2015-09-15 11:39:16 +0800

    Init commit

---
+++ Psychologist/DiagnosedHappinessViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DiagnosedHappinessViewController.swift
+//  Psychologist
+//
+//  Created by nevercry on 9/14/15.
+//  Copyright (c) 2015 nevercry. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DiagnosedHappinessViewController : HappinessViewController,UIPopoverPresentationControllerDelegate {
+    
+    override var happiness: Int {
+        didSet {
+            diagnosticHistroy += [happiness]
+        }
+    }
+    
+    private let defaults = NSUserDefaults.standardUserDefaults()
+    var diagnosticHistroy : [Int] {
+        get { return defaults.objectForKey(History.DefaultsKey) as? [Int] ?? [] }
+        set { defaults.setObject(newValue, forKey: History.DefaultsKey) }
+    }
+    
+    private struct History {
+        static let SegueIdentifier = "Show Diagnostic History"
+        static let DefaultsKey = "DiagnosedHappinessViewController.History"
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case History.SegueIdentifier:
+                if let tvc = segue.destinationViewController as? TextViewController {
+                    if let ppc = tvc.popoverPresentationController {
+                        ppc.delegate = self
+                    }
+                    tvc.text = "\(diagnosticHistroy)"
+                }
+            default: break
+            }
+        }
+    }
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.None
+    }
+    
+}
+
